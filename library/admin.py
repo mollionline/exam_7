b29099b10307751037bd74e79df9645150cb97e0
moldir <@auezova_moldir>
2022-02-12 15:57:23 +0600

    Добавила в админку автора

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from library.models import Book
+from library.models import Book, Author
 
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'birthday', 'death_date', 'description']
+    list_filter = ['author']
+    search_fields = ['author']
+    fields = ['id', 'author', 'birthday', 'death_date', 'description']
+    readonly_fields = ['id']
+
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ['id', 'book', 'description', 'author', 'published_at']
@@ -13,3 +20,4 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
