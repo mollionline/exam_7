3b634fd837ca79bee2f7594a75931af56c3037ed
moldir <@auezova_moldir>
2022-02-12 15:58:08 +0600

    Добавила в формы и в модели автора

--- library/forms.py
+++ library/forms.py
@@ -1,12 +1,24 @@
 from django import forms
-from library.models import Book
+from library.models import Book, Author
 
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['book', 'description', 'author', 'published_at']
+        fields = ['book', 'description', 'published_at']
+
+
+class AuthorForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = ['author', 'birthday', 'death_date', 'description']
 
 
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=60, required=False)
+
+
+class BookWithoutAuthorForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['book', 'description', 'published_at', 'author']
--- library/models.py
+++ library/models.py
@@ -2,8 +2,24 @@ from django.db import models
 
 
 # Create your models here.
+class Author(models.Model):
+    author = models.CharField(max_length=50, null=False, blank=False)
+    birthday = models.DateTimeField(null=True, blank=True)
+    death_date = models.DateTimeField(null=True, blank=True)
+    description = models.TextField(max_length=1000, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.author}"
+
+
 class Book(models.Model):
     book = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(max_length=1000, null=True, blank=True)
-    author = models.CharField(max_length=40, null=False, blank=False)
+    author = models.ForeignKey(
+        'library.Author',
+        related_name='books',
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        default=1
+    )
     published_at = models.DateTimeField(null=True, blank=False)
